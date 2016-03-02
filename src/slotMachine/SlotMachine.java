4097a82bd9480f7b18fe05ff2259efbd0f1e9c36
bassoandrea <bassoandrea@Lab06_17.itseinaudi.local>
2016-03-02 08:57:30 +0100

    qwerty

--- src/slotMachine/SlotMachine.java
+++ src/slotMachine/SlotMachine.java
@@ -13,13 +13,13 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
-
 public class SlotMachine {
 
 	protected Shell shell;
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
 	
 	ArrayList <String> img = new ArrayList <String>();	
+	int n;
 	/**
 	 * Launch the application.
 	 * @param args
@@ -57,27 +57,38 @@ public class SlotMachine {
 		shell.setSize(506, 423);
 		shell.setText("SWT Application");
 		
+		img.add("/img/Anguria.jpg");
+		img.add("/img/Arancia.jpg");
+		img.add("/img/Banana.jpg");
+		img.add("/img/Bar.jpg");
+		img.add("/img/Ciliegia.jpg");
+		img.add("/img/Limone.jpg");
+		img.add("/img/Mela.jpg");
+		img.add("/img/Uva.jpg");
 		
+		Label lbl1 = formToolkit.createLabel(shell, "", SWT.NONE);
+		lbl1.setBounds(10, 95, 150, 150);
+		
+		Label lbl2 = formToolkit.createLabel(shell, "", SWT.NONE);
+		lbl2.setBounds(166, 95, 150, 150);
+		
+		Label lbl3 = formToolkit.createLabel(shell, "", SWT.NONE);
+		lbl3.setBounds(322, 95, 158, 150);
 		
 		Button btnGira = new Button(shell, SWT.NONE);
 		btnGira.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				
+				n = (int)(Math.random()*8);
+				//lbl1.setImage(SWTResourceManager.getImage(main.class, img.get(n)));
 			}
 		});
 		btnGira.setBounds(405, 350, 75, 25);
 		btnGira.setText("Gira");
 		
-		Label lblNewLabel = formToolkit.createLabel(shell, "", SWT.NONE);
-		lblNewLabel.setBounds(10, 95, 150, 150);
 		
-		Label lblNewLabel_1 = formToolkit.createLabel(shell, "", SWT.NONE);
-		lblNewLabel_1.setBounds(166, 95, 150, 150);
+
 		
-		Label lblNewLabel_2 = formToolkit.createLabel(shell, "", SWT.NONE);
-		lblNewLabel_2.setBounds(322, 95, 158, 150);
-
-
 	}
 }
