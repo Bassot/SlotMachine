66ac3251ca80b50e286098f9bea15e75f6288744
bassoandrea <bassoandrea@Lab06_17.itseinaudi.local>
2016-03-03 12:13:00 +0100

    Fine 3 img

--- src/slotMachine/SlotMachine.java
+++ src/slotMachine/SlotMachine.java
@@ -15,19 +15,18 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.wb.swt.SWTResourceManager;
 
-
 public class SlotMachine {
-
 
 	protected Shell shell;
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-	
-	ArrayList <String> img = new ArrayList <String>();	
+
+	ArrayList<String> img = new ArrayList<String>();
 
 	int n;
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -60,40 +59,67 @@ public class SlotMachine {
 	protected void createContents() {
 
 		shell = new Shell();
-		shell.setSize(506, 423);
+		shell.setSize(564, 487);
 		shell.setText("SWT Application");
-		
-		img.add("/img/Anguria.jpg");
-		img.add("/img/Arancia.jpg");
-		img.add("/img/Banana.jpg");
-		img.add("/img/Bar.jpg");
-		img.add("/img/Ciliegia.jpg");
-		img.add("/img/Limone.jpg");
-		img.add("/img/Mela.jpg");
-		img.add("/img/Uva.jpg");
-		
+
+		img.add("/slotMachine/img/Anguria.jpg");
+		img.add("/slotMachine/img/Arancia.jpg");
+		img.add("/slotMachine/img/Banana.jpg");
+		img.add("/slotMachine/img/Bar.jpg");
+		img.add("/slotMachine/img/Ciliegia.jpg");
+		img.add("/slotMachine/img/Limone.jpg");
+		img.add("/slotMachine/img/Mela.jpg");
+		img.add("/slotMachine/img/Uva.jpg");
+
 		Label lbl1 = formToolkit.createLabel(shell, "", SWT.NONE);
-		lbl1.setImage(SWTResourceManager.getImage(SlotMachine.class, "/slotMachine/img/Anguria.jpg"));
-		lbl1.setBounds(10, 95, 150, 150);
-		
+
+		lbl1.setBounds(10, 95, 177, 170);
+
 		Label lbl2 = formToolkit.createLabel(shell, "", SWT.NONE);
-		lbl2.setBounds(166, 95, 150, 150);
-		
+		lbl2.setBounds(193, 95, 181, 170);
+
 		Label lbl3 = formToolkit.createLabel(shell, "", SWT.NONE);
-		lbl3.setBounds(322, 95, 158, 150);
-		
+		lbl3.setBounds(380, 95, 168, 170);
+
 		Button btnGira = new Button(shell, SWT.NONE);
 		btnGira.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
-				n = (int)(Math.random()*8);
+
+				Thread t = new Thread() {
+					public void run() {
+						for (int i = 0; i < 10; i++) {
+							
+							Display.getDefault().asyncExec(	new Runnable() {
+								@Override
+								public void run() {
+									// TODO Auto-generated method stub
+									n = (int) (Math.random() * 8);
+									lbl1.setImage(SWTResourceManager.getImage(SlotMachine.class, img.get(n)));
+									n = (int) (Math.random() * 8);
+									lbl2.setImage(SWTResourceManager.getImage(SlotMachine.class, img.get(n)));
+									n = (int) (Math.random() * 8);
+									lbl3.setImage(SWTResourceManager.getImage(SlotMachine.class, img.get(n)));
+									
+								}
+								
+							});
+							
+							try {
+								Thread.sleep(100); // 1000 milliseconds is one
+													// second.
+							} catch (InterruptedException ex) {
+								Thread.currentThread().interrupt();
+							}
+
+						}
+					}
+				};
+				t.start();
 			}
 		});
 		btnGira.setBounds(405, 350, 75, 25);
 		btnGira.setText("Gira");
 
-
-		
 	}
 }
